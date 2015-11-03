c7a5c87ecd4cf993ac4705370ea0293b684ceb93
Kareemulla Shaik <kareemulla.shaik@cdk.com>
2015-11-03 14:07:40 +0530

    Readline statement added

--- SampleGitApp/Program.cs
+++ SampleGitApp/Program.cs
@@ -12,6 +12,7 @@ namespace SampleGitApp
         {
             Console.WriteLine("Sample Git Application...");
             Console.WriteLine("Second change..");
+            Console.ReadLine();
         }
     }
 }
