33b16f369c3b2aa77e22317255c2fad027309c71
Kareemulla <kareemulla96@hotmail.com>
2015-11-03 14:23:13 +0530

    Update Program.cs

--- SampleGitApp/Program.cs
+++ SampleGitApp/Program.cs
@@ -12,7 +12,7 @@ namespace SampleGitApp
         {
             Console.WriteLine("Sample Git Application...");
             Console.WriteLine("Second change..");
-            
+            Console.WriteLine("Undo changes");
             //Edited using git-hub site
             Console.ReadLine();
 
